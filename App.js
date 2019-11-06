65b42a59335ff9389742a460ccd08fcccb27aa1c
Ikkyy <msndoikky@hotmail.com>
2019-11-06 17:34:22 -0300

    Configuração Inicial

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Debugaaae</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+
+});
+
+export default App;
