06990708bfd9726c2215836c213f6795b6a21123
Ikkyy <msndoikky@hotmail.com>
2019-11-06 17:55:06 -0300

    Reactotron

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+import './config/ReactotronConfig'
+
+console.tron.log('Olá')
+
+const styles = StyleSheet.create({
+
+
+});
+
+export default function App() {
+  return (
+    <>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Debuga3aae</Text>
+      </View>
+    </>
+  );
+};
+
+
