e0c63d3092da7c8b3939ac2d591d304a0faa49ec
Ikkyy <msndoikky@hotmail.com>
2019-11-07 14:02:48 -0300

    Navigation

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
+import { View } from 'react-native';
 
 import './config/ReactotronConfig'
-
-console.tron.log('Olá')
-
-const styles = StyleSheet.create({
-
-
-});
+import Routes from './routes'
 
 export default function App() {
-  return (
-    <>
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Debuga3aae</Text>
-      </View>
-    </>
-  );
+    return <Routes />;
 };
 
 
