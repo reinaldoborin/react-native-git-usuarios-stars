cec40235165bf8ecf917a13232f2606ab255c6dc
Ikkyy <msndoikky@hotmail.com>
2019-11-07 14:15:47 -0300

    Styled

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
+import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Main from './pages/Main'
 import User from './pages/User'
-
-
 
 const Routes  = createAppContainer(createStackNavigator({
     Main,
