01864ccfaf77268e0a1d43b1d0c964cb5920a6f4
Ikkyy <msndoikky@hotmail.com>
2019-11-07 14:55:47 -0300

    Estilizando Listagem de Usuários

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,7 +4,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import api from '../../services/api'
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import { Container, Form, Input, SubmitButton, List, Avatar, Name, Bio, User, ProfileButton, ProfileButtonText } from './styles';
 
 export default class Main extends Component {
 
@@ -25,7 +25,7 @@ export default class Main extends Component {
         }
 
         this.setState({
-            users: [... users, data],
+            users: [...users, data],
             newUser: ''
         })
 
@@ -51,6 +51,21 @@ export default class Main extends Component {
                         <Icon name="add" size={20} color="#FFF" />
                     </SubmitButton>
                 </Form>
+
+                <List
+                    data={users}
+                    keyExtractor={user => user.login}
+                    renderItem={({ item }) => (
+                        <User>
+                            <Avatar source={{ uri: item.avatar }} />
+                            <Name>{item.name}</Name>
+                            <Bio>{item.bio}</Bio>
+                            <ProfileButton onPress={() => { }}>
+                                <ProfileButtonText>Ver Perfil</ProfileButtonText>
+                            </ProfileButton>
+                        </User>
+                    )}
+                />
             </Container>
         )
     }
